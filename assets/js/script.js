a1a945d4c6a782e2c277e589fff53dedbc5b8356
abbyd <abbydiaz300@gmail.com>
2023-06-15 18:11:53 -0700

    added date handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -109,6 +111,15 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jQuery ui datepicker
+  dateInput.datepicker({
+    minDate:1,
+    onclose: function() {
+      // when calendar is closed, force a ' change ' event on the dateInput 
+      $(this).trigger("change");
+    }
+  })
+
   // automatically focus on new element 
   dateInput.trigger("focus");
 });
@@ -116,7 +127,7 @@ $(".list-group").on("click", "span", function() {
 // convert back when user clicks out 
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
   .val()
@@ -144,6 +155,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element 
   $(this).replaceWith(taskSpan);
+
+  // pass tasks li element into auditTask() to check new due date
+  auditTask($(taskSpan).closest('.list-group-item'));
 });
 
 // .sortable() turned every element with the class list-group into a sortable list
@@ -207,6 +221,7 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tollerance: "touch",
   drop: function(event, ui) {
+    // removes the item when dropped into the red zone 
     ui.draggable.remove();
     console.log("drop");
   },
@@ -217,6 +232,36 @@ $("#trash").droppable({
     console.log('out');
   }
 });
+
+// for date in modal
+$('#modalDueDate').datepicker({
+  // set min date to be one day from the current day 
+  minDate: 1
+});
+
+var auditTask = function(taskEl) {
+  // get date from task element 
+  var date = $(taskEl).find('span')
+  .text()
+  .trim();
+
+  // convert to moment object at 5pm
+  var time = moment(date, "L").set('hour', 17);
+  // remove any old classes from element 
+  $(taskEl).removeClass('list-group-item-warning list-group-item-danger');
+
+  // apply new class if task is near/over due date 
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass('list-group-item-danger');
+  }
+  // use moment to get 'right now'.... use .diff after to get the difference
+  // of right now to a day in the future, get a negative number back 
+  // think when people say 't minus ten seconds '
+  // wrapped returning val in the moment.diff in the .abs so we get the absolute value of the number
+  else if (Math.abs(moment().diff(time, 'days')) <= 2) {
+    $(taskEl).addClass('list-group-item-warning');
+  }
+};
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
